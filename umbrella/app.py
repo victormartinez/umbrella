b7410199df2e2da5d71547c2a12caf57717e5df6
Victor Martinez <vcrmartinez@gmail.com>
2020-05-18 23:59:25 -0300

    feat: use argparse to take params as coords

--- umbrella/app.py
+++ umbrella/app.py
@@ -1,3 +1,5 @@
+import argparse
+
 from .service import UmbrellaService
 from .ui import ReportPrinter
 from .openweather.client import DailyForecastCall
@@ -16,4 +18,16 @@ def execute(coords):
 
 
 if __name__ == "__main__":
-    execute((1, 2))
+    parser = argparse.ArgumentParser(description="Should you take a umbrella?")
+    parser.add_argument(
+        "--lat", metavar="lat", type=float, nargs=1, required=True, help="Latitude"
+    )
+    parser.add_argument(
+        "--lon", metavar="lon", type=float, nargs=1, required=True, help="Longitude"
+    )
+
+    args = parser.parse_args()
+    lat, lon = args.lat[0], args.lon[0]
+
+    print(f"Latitude: {lat} Longitude: {lon}")
+    execute((lat, lon))
