abe50bfeb96d32f11263c42039e492d7192338c4
Victor Martinez <vcrmartinez@gmail.com>
2020-05-18 23:13:53 -0300

    feat: create UmbrellaService to determine usage

---
+++ umbrella/service.py
@@ -0,0 +1,13 @@
+class UmbrellaService:
+
+    HUMIDITY_THRESHOLD = 70
+
+    def __init__(self, forecasts):
+        self.forecasts = forecasts
+
+    def report(self):
+        return [
+            {"dt": f["dt"], "umbrella": f["humidity"] > self.HUMIDITY_THRESHOLD}
+            for f in self.forecasts
+        ]
+
