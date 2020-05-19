b56d9f1dbf749a05d36966eb20cfd1adee7a45dd
Victor Martinez <vcrmartinez@gmail.com>
2020-05-18 23:35:52 -0300

    feat: pretty print if you should take umbrella

--- umbrella/ui.py
+++ umbrella/ui.py
@@ -1,7 +1,23 @@
+from datetime import datetime
+
+
 class ReportPrinter:
+
+    INLINE_NO_DAYS = "There is not need of umbrella for the next days."
+    INLINE_TEMPLATE = "You should take an umbrella in these days: {}"
 
     def __init__(self, report):
         self.report = report
 
     def print_inline(self):
-        print(self.report)
+        umbrella_usage_days = [
+            datetime.strftime(row["dt"], "%A")
+            for row in self.report
+            if row["umbrella"]
+        ]
+
+        if umbrella_usage_days:
+            days_str = ", ".join(umbrella_usage_days)
+            print(self.INLINE_TEMPLATE.format(days_str))
+        else:
+            print(self.INLINE_NO_DAYS)
